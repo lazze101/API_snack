7c4e9ef03385de83aeec745e4758051de3289a4c
lazze101 <giulio.lazzerini@polomanettiporciatti.edu.it>
2025-01-21 09:40:44 +0100

    finito

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const menù = [
         type: 'list',
         name: 'menù',
         message: 'Cosa vuoi fare?',
-        choices: ['Visualizza prodotti', 'Inserisci prodotto', 'Esci']
+        choices: ['Inserisci snack', 'visuallizza snack dal nome', 'visuallizza snack dalla categoria','visuallizza snack dalle calorie', 'Esci']
     }
 ];
 
@@ -14,7 +14,7 @@ const domandeInserimento = [
     {
         type: 'input',
         name: 'nome',
-        message: 'Nome prodotto:',
+        message: 'Nome snack:',
         validate: (value) => {
             if (value.length > 0) {
                 return true;
@@ -23,10 +23,24 @@ const domandeInserimento = [
             }
         }
     },
+
+    {
+        type: 'input',
+        name: 'categoria',
+        message: 'Categoria snack:',
+        validate: (value) => {
+            if (value.length > 0) {
+                return true;
+            } else {
+                return 'Inserisci una categoria!';
+            }
+        }
+    },
+
     {
         type: 'number',
         name: 'prezzo',
-        message: 'Prezzo prodotto:',
+        message: 'Prezzo snack:',
         validate: (value) => {
             if (value >= 0) {
                 return true;
@@ -37,13 +51,25 @@ const domandeInserimento = [
     },
     {
         type: 'number',
-        name: 'quantità',
-        message: 'Quantità prodotto:',
+        name: 'peso',
+        message: 'peso snack:',
         validate: (value) => {
             if (value >= 0) {
                 return true;
             } else {
-                return 'Inserisci una quantità non negativa!';
+                return 'Inserisci un peso non negativa!';
+            }
+        }
+    },
+    {
+        type: 'number',
+        name: 'calorie',
+        message: 'calorie per 100 grammi snack:',
+        validate: (value) => {
+            if (value >= 0) {
+                return true;
+            } else {
+                return 'Inserisci quantità calorie non negativa!';
             }
         }
     }
@@ -52,24 +78,59 @@ const domandeInserimento = [
 function main() {
     inquirer.prompt(menù).then((answers) => {
         switch(answers.menù) {
-            case 'Visualizza prodotti':
-                axios.get('http://localhost:3000/api/prodotti').then((response) => {
-                    console.log(response.data);
-                });
-                break;
-            case 'Inserisci prodotto':
+            case 'Inserisci snack':
                 inquirer.prompt(domandeInserimento).then((answers) => {
-                    axios.post('http://localhost:3000/api/prodotto', {
+                    axios.put('http://localhost:3000/snack', {
                         nome: answers.nome,
+                        categoria: answers.categoria,
                         prezzo: answers.prezzo,
-                        quantità: answers.quantità
+                        peso: answers.peso,
+                        calorie: answers.calorie
                     }).then((response) => {
-                        console.log(response.data);        
+                        console.log(response.data);
                     }).catch((err) => {
                         console.log(err.response.data);
                     });
                 });
                 break;
+            case 'visuallizza snack dal nome':
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'nome',
+                    message: 'Nome snack:'
+                }).then((answers) => {
+                    axios.get(`http://localhost:3000/snack/${answers.nome}`).then((response) => {
+                        console.log(response.data);
+                    }).catch((err) => {
+                        console.log(err.response.data);
+                    });
+                });
+                break;
+            case 'visuallizza snack dalla categoria':
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'categoria',
+                    message: 'Categoria snack:'
+                }).then((answers) => {
+                    axios.get(`http://localhost:3000/cat/${answers.categoria}`).then((response) => {
+                        console.log(response.data);
+                    }).catch((err) => {
+                        console.log(err.response.data);
+                    });
+                });
+                break;
+            case 'visuallizza snack dalle calorie':
+                inquirer.prompt({
+                    type: 'number',
+                    name: 'calorie',
+                    message: 'Calorie snack:'
+                }).then((answers) => {
+                    axios.get(`http://localhost:3000/cal/${answers.calorie}`).then((response) => {
+                        console.log(response.data);
+                    }).catch((err) => {
+                        console.log(err.response.data);
+                    });
+                });
             case 'Esci':
                 console.log("Bye!");
                 return;
